a1c470b5644fd523031835793089b7bff72b3b27
Hatiper <001756@Muzaffers-MacBook-Pro.local>
2014-05-29 13:21:40 +0300

    Second Commit

---
+++ OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out OF The World
+//
+//  Created by 001756 on 28/05/14.
+//  Copyright (c) 2014 MuzafferHatiper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (nonatomic,strong) NSMutableArray *planets;
+
+@end
